99ffd39a227182385c5dbc54b85dfffb2788c5a2
Paul Phyo <paulphyo25@gmail.com>
2025-04-15 19:15:34 +0800

    feat: added nav bar & book list page

--- react-frontend/src/layouts/Layout.tsx
+++ react-frontend/src/layouts/Layout.tsx
@@ -1,7 +1,19 @@
+import NavBar from "../components/NavBar";
+import { Outlet } from 'react-router-dom';
+
 const Layout = () => {
     return (
-        <>
-        </>
+        <div>
+            <header>
+                <NavBar></NavBar>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+            </footer>
+        </div>
     );
-}
+};
+
 export default Layout;
