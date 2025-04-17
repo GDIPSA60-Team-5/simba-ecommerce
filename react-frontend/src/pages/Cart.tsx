3060955dfcdf724dde2e731a22a39c57202ac0f3
Aung Myin Moe <ag.mmoe@gmail.com>
2025-04-17 17:55:36 +0800

    solved some merge conflicts

--- react-frontend/src/pages/Cart.tsx
+++ react-frontend/src/pages/Cart.tsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import ListCartItem from "../feature/cart/component/ListCartItem";
-=======
-import ListCartItem from "../feature/cart/components/ListCartItem"
->>>>>>> d7e76bb (integrating cart to UI page)
 
 const Cart = () => {
     return (
         <>
-<<<<<<< HEAD
             <ListCartItem />
-=======
-            <ListCartItem></ListCartItem>
->>>>>>> d7e76bb (integrating cart to UI page)
         </>
     );
 }
