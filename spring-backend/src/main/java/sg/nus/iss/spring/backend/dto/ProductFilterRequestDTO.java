73b2a82cadbaed8b03585d1d69b747e8d66e47bf
Paul Phyo <paulphyo25@gmail.com>
2025-04-18 01:47:22 +0800

    fix: replaced lombok annotations with traditional getters & setters

--- spring-backend/src/main/java/sg/nus/iss/spring/backend/dto/ProductFilterRequestDTO.java
+++ spring-backend/src/main/java/sg/nus/iss/spring/backend/dto/ProductFilterRequestDTO.java
@@ -1,10 +1,5 @@
 package sg.nus.iss.spring.backend.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class ProductFilterRequestDTO {
     private String sortBy = "name";
     private String sortDir = "ASC";
@@ -16,4 +11,68 @@ public class ProductFilterRequestDTO {
     private Float minPrice;
     private Float maxPrice;
     private String keywords;
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public String getSortDir() {
+        return sortDir;
+    }
+
+    public void setSortDir(String sortDir) {
+        this.sortDir = sortDir;
+    }
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public Integer getItemsPerPage() {
+        return itemsPerPage;
+    }
+
+    public void setItemsPerPage(Integer itemsPerPage) {
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Float getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(Float minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public Float getMaxPrice() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(Float maxPrice) {
+        this.maxPrice = maxPrice;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
 }
