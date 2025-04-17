42b62236b8c929f87eac138d3448aaac4fe0915c
Aung Myin Moe <ag.mmoe@gmail.com>
2025-04-18 06:34:07 +0800

    format stripe payment form and make payment button

--- react-frontend/src/feature/stripe/components/StripeCheckoutForm.tsx
+++ react-frontend/src/feature/stripe/components/StripeCheckoutForm.tsx
@@ -1,16 +1,19 @@
 import { useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
-
+import { useState } from 'react';
 
 // Written by Aung Myin Moe
 const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [isProcessing, setIsProcessing] = useState(false);
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
 
         if (!stripe || !elements) return;
 
+        setIsProcessing(true);
+        
         const result = await stripe.confirmPayment({
             elements,
             confirmParams: {
@@ -22,13 +25,21 @@ const CheckoutForm = () => {
             console.error(result.error.message);
             alert("Payment failed. Please try again.");
         }
+
+        setIsProcessing(false);
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <PaymentElement />
-            <button type="submit" disabled={!stripe}>Submit Payment</button>
-        </form>
+        <main>
+            <form id="payment-form" onSubmit={handleSubmit}>
+                <PaymentElement />
+                <button disabled={isProcessing} id="submit" className="stripe-button">
+                    <span id="button-text">
+                        {isProcessing ? "Processing ... " : "Pay now"}
+                    </span>
+                </button>
+            </form>
+        </main>
     );
 };
 
