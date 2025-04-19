a9a308dc3b05842c585a817a5b53ba2fa1682b78
Aung Myin Moe <ag.mmoe@gmail.com>
2025-04-19 20:44:13 +0800

    added cart validation and cart frontend UI

--- react-frontend/src/feature/cart/components/ListCartItem.tsx
+++ react-frontend/src/feature/cart/components/ListCartItem.tsx
@@ -194,19 +194,46 @@ export default function ListCartItem() {
             </div>
 
             <div className="flex justify-between gap-8 mb-8">
-                {/* Table on the left */}
-                <table className="w-[70%] mr-6 border-collapse">
-                    <thead>
-                    <tr className="text-sm text-gray-600 font-medium text-center pb-4">
-                        <th className="w-1/5 pb-4">BOOK</th>
-                        <th className="w-1/5 pb-4">PRICE (SGD)</th>
-                        <th className="w-1/5 pb-4">QUANTITY</th>
-                        <th className="w-1/5 pb-4">TOTAL (SGD)</th>
-                        <th className="w-1/5 pb-4">ACTION</th>
-                    </tr>
-                    </thead>
-                    <tbody>{listCartHtml}</tbody>
-                </table>
+                <div className="w-[70%]">
+                    {/* Table on the left */}
+                    <table className="w-full mr-7 border-collapse">
+                        <thead>
+                        <tr className="text-sm text-gray-600 text-center pb-4">
+                            <th className="w-1/5 pb-4 font-normal">BOOK</th>
+                            <th className="w-1/5 pb-4 font-normal">PRICE (SGD)</th>
+                            <th className="w-1/5 pb-4 font-normal">QUANTITY</th>
+                            <th className="w-1/5 pb-4 font-normal">TOTAL (SGD)</th>
+                            <th className="w-1/5 pb-4 font-normal">REMOVE</th>
+                        </tr>
+                        </thead>
+                        <tbody>{listCartHtml}</tbody>
+                    </table>
+                    <div className="flex gap-6 mt-6">
+                        <button type="button" onClick={handleSaveClick} disabled={isSaving || !cartChanged}
+                            className={`px-6 py-4 text-white transition-colors duration-300 
+                                ${isSaving ? 'bg-gray-300 cursor-not-allowed' : cartChanged ? 'bg-green-500 hover:bg-green-600' : 'bg-gray-400 cursor-not-allowed'}`} 
+                        >
+                            {isSaving ? (
+                                <div className="flex items-center gap-2">
+                                <svg className="animate-spin h-4 w-4 text-white" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z" />
+                                </svg>
+                                    Saving...
+                                </div>
+                            ) : (
+                                "SAVE CART"
+                            )}
+                        </button>
+                        <button
+                            type="button"
+                            onClick={handleDeleteCart}
+                            className="py-4 px-6 bg-red-600 text-white border-none hover:bg-red-700 active:bg-red-800 transition duration-200"
+                            >
+                            DELETE CART
+                        </button>
+                    </div>
+                </div>
                 
                 {/* Totals on the right with delivery inputs */}
                 <div className="w-[30%] min-w-[220px] border-t-18 border-black ml-5 mt-2 pt-4">
@@ -228,157 +255,100 @@ export default function ListCartItem() {
                                 <td></td>
                             </tr>
                             <tr>
-                                <td className="border-t border-gray-400 text-left py-4 font-medium text-gray-800">TOTAL</td>
-                                <td className="border-t border-gray-400 text-right py-4 font-medium text-gray-800">
+                                <td className="border-t border-gray-400 text-left py-4 font-bold text-gray-800">TOTAL</td>
+                                <td className="border-t border-gray-400 text-right py-4 font-bold text-gray-800">
                                     SGD {(calculateTotal() + calculateTax() + deliveryFee).toFixed(2)}
                                 </td>
                                 <td></td>
                             </tr>
                         </tbody>
                     </table>
-            {validationErrors.cart && (
-                <div style={{ color: "red", marginBottom: "1rem" }}>
-                    {validationErrors.cart}
-                </div>
-            )}
-                </div>
-            </div>
-
-            {/* Delivery Type
-            <div className="mt-8 mb-10">
-            <label htmlFor="deliveryType" className="block text-sm text-gray-600 font-medium mb-2 tracking-wide uppercase">
-                Delivery Type
-            </label>
-            <select
-                id="deliveryType"
-                name="deliveryType"
-                ref={deliTypeElement}
-                required
-                className="w-full p-3 bg-white border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition ease-in-out"
-                value={selectedDeliveryType ? selectedDeliveryType.name : ""}
-                onChange={(e) => {
-                const selectedName = e.target.value;
-                const selected = deliType.find(type => type.name === selectedName) || null;
-                setSelectedDeliveryType(selected);
-                setDeliveryFee(selected ? selected.fee : 0);
-                sessionStorage.setItem("deliveryType", JSON.stringify(selected));
-                }}
-            >
-                <option value="" className="text-gray-400">
-                    Please select a delivery type
-                </option>
-                {Array.isArray(deliType) &&
-                deliType.map((type) => (
-                    <option key={type.id} value={type.name}>
-                        {`${type.name} (${type.fee.toFixed(2)} SGD) - ${type.description}`}   
-                    </option>
-                ))}
-            </select>
-            </div> */}
-
-            <div className="mt-8 mb-10">
-            <label
-                htmlFor="deliveryType"
-                className="block text-sm text-gray-600 font-medium mb-2 tracking-wide uppercase"
-            >
-                Delivery Type
-            </label>
-            <select
-                id="deliveryType"
-                name="deliveryType"
-                ref={deliTypeElement}
-                required
-                className="w-full p-3 bg-white border border-gray-300 rounded-lg shadow-sm text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-gray-400 transition ease-in-out"
-                value={selectedDeliveryType ? selectedDeliveryType.name : ""}
-                onChange={(e) => {
-                const selectedName = e.target.value;
-                const selected = deliType.find(type => type.name === selectedName) || null;
-                setSelectedDeliveryType(selected);
-                setDeliveryFee(selected ? selected.fee : 0);
-                sessionStorage.setItem("deliveryType", JSON.stringify(selected));
-                }}
-            >
-                <option value="" className="text-gray-400">
-                Please select a delivery type
-                </option>
-                {Array.isArray(deliType) &&
-                deliType.map((type) => (
-                    <option
-                    key={type.id}
-                    value={type.name}
-                    className="text-gray-700 bg-white hover:bg-gray-100" // `hover:bg-*` often has limited support for `<option>`
-                    >
-                    {`${type.name} (${type.fee.toFixed(2)} SGD) - ${type.description}`}
-                    </option>
-                ))}
-            </select>
-                {validationErrors.deliveryType && (
-                <div style={{ color: "red", marginTop: "0.5rem" }}>
-                    {validationErrors.deliveryType}
-                </div>
-            )}
-
-
-            </div>
-
-
-
-            {/* Shipping Address */}
-            <div className="mt-8 mb-10">
-            <label htmlFor="shippingAddress" className="block text-sm text-gray-600 font-medium mb-2 tracking-wide uppercase">
-                Shipping Address
-            </label>
-            <textarea
-                id="shippingAddress"
-                name="shippingAddress"
-                rows={4}
-                placeholder="Enter your shipping address"
-                ref={shippingAddressElement}
-                required
-                className="w-full p-3 bg-white border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition ease-in-out resize-none"
-                onChange={(e) => {
-                sessionStorage.setItem("shippingAddress", e.target.value);
-                }}
-            ></textarea>
-                {validationErrors.shippingAddress && (
-                    <div style={{ color: "red", marginTop: "0.5rem" }}>
-                        {validationErrors.shippingAddress}
-                    </div>
-                )}
-
-            </div>
-
-            <div style={{ display: "flex", gap: "1rem" }}>
-                <button
-                    type="button"
-                    onClick={handleSubmitOrder}
-                    className="px-6 py-4 bg-black text-white border-none rounded-none hover:bg-gray-800 active:bg-gray-900 transition duration-200"
-                    >
-                    CHECKOUT
-                </button>
-                <button type="button" onClick={handleSaveClick} disabled={isSaving || !cartChanged}
-                    className={`px-6 py-4 text-white transition-colors duration-300 
-                        ${isSaving ? 'bg-gray-300 cursor-not-allowed' : cartChanged ? 'bg-gray-500 hover:bg-gray-600' : 'bg-gray-400 cursor-not-allowed'}`} 
-                >
-                    {isSaving ? (
-                        <div className="flex items-center gap-2">
-                        <svg className="animate-spin h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z" />
-                        </svg>
-                            Saving...
+                    {validationErrors.cart && (
+                        <div style={{ color: "red", marginBottom: "1rem" }}>
+                            {validationErrors.cart}
                         </div>
-                    ) : (
-                        "SAVE CART"
                     )}
-                </button>
-                <button
-                    type="button"
-                    onClick={handleDeleteCart}
-                    className="py-4 px-6 bg-red-600 text-white border-none hover:bg-red-700 active:bg-red-800 transition duration-200"
-                    >
-                    DELETE CART
-                </button>
+
+                    {/* Delivery Type */}
+                    <div className="w-full mt-8 mb-10">
+                        <label
+                        htmlFor="deliveryType"
+                        className="block text-sm text-gray-600 font-normal mb-2 tracking-wide uppercase"
+                        >
+                        Delivery Type
+                        </label>
+                        <select
+                        id="deliveryType"
+                        name="deliveryType"
+                        ref={deliTypeElement}
+                        required
+                        className="w-full p-3 bg-white border border-gray-300 rounded-sm shadow-sm text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-gray-400 transition ease-in-out"
+                        value={selectedDeliveryType ? selectedDeliveryType.name : ""}
+                        onChange={(e) => {
+                            const selectedName = e.target.value;
+                            const selected = deliType.find(type => type.name === selectedName) || null;
+                            setSelectedDeliveryType(selected);
+                            setDeliveryFee(selected ? selected.fee : 0);
+                            sessionStorage.setItem("deliveryType", JSON.stringify(selected));
+                        }}
+                        >
+                        <option value="" className="text-gray-400">
+                            -- Please select a delivery type --
+                        </option>
+                        {Array.isArray(deliType) &&
+                            deliType.map((type) => (
+                            <option
+                                key={type.id}
+                                value={type.name}
+                                className="text-gray-700 bg-white"
+                            >
+                                {`${type.name} (SGD ${type.fee.toFixed(2)}) -- ${type.description} --`}
+                            </option>
+                            ))}
+                        </select>
+                        {validationErrors.deliveryType && (
+                        <div className="text-red-600 mt-2 text-sm">
+                            {validationErrors.deliveryType}
+                        </div>
+                        )}
+                    </div>
+
+                    {/* Shipping Address */}
+                    <div className="w-full mt-8 mb-4">
+                        <label
+                            htmlFor="shippingAddress"
+                            className="block text-sm text-gray-600 font-normal mb-2 tracking-wide uppercase"
+                        >
+                            Shipping Address
+                        </label>
+                        <textarea
+                            id="shippingAddress"
+                            name="shippingAddress"
+                            rows={4}
+                            placeholder="Enter your shipping address"
+                            ref={shippingAddressElement}
+                            required
+                            className="w-full p-3 bg-white border border-gray-300 rounded-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-gray-400 transition ease-in-out resize-none"
+                            onChange={(e) => {
+                                sessionStorage.setItem("shippingAddress", e.target.value);
+                            }}
+                        ></textarea>
+                        {validationErrors.shippingAddress && (
+                            <div className="text-red-600 mt-2 text-sm">
+                                {validationErrors.shippingAddress}
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        <button
+                            type="button"
+                            onClick={handleSubmitOrder}
+                            className="px-6 py-4 bg-black text-white border-none rounded-none hover:bg-gray-800 active:bg-gray-900 transition duration-200"
+                            >
+                            CHECKOUT
+                        </button>
+                    </div>
+                </div>
             </div>
         </form>    
     )
